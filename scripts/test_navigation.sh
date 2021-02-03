bff658dc5102fcdc998767c8346de62fcd048ae2
David Smith <davidsmithxc@gmail.com>
2021-02-02 19:58:42 -0800

    SLAM, nav test scripts. pick_objects node - fixed robot initial pose and estimate - added pick objects node with two basic nav goals.

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,15 @@
+xterm -e "cd /home/robond/workspace/catkin_ws/;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x 4 -y -3 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/robond/workspace/catkin_ws/src/map/apartment_ball.world" &
+sleep 5
+
+xterm -e "cd /home/robond/workspace/catkin_ws/;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/robond/workspace/catkin_ws/src/map/map.yaml" &
+sleep 5
+
+xterm -e "cd /home/robond/workspace/catkin_ws/;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5

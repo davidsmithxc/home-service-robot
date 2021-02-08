1706c1bd216d1db893c672e741ee8c657681d436
David Smith <davidsmithxc@gmail.com>
2021-02-08 07:30:21 -0800

    added test script for pick_objects

---
+++ scripts/pick_objects.sh
@@ -0,0 +1,19 @@
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+export ROBOT_INITIAL_POSE='-x 4 -y -3 -z 0 -R 0 -P 0 -Y 0';
+roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/src/map/apartment_ball.world" &
+sleep 5
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/src/map/map.yaml" &
+sleep 5
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 10
+
+xterm -e "cd $(pwd)/../..;
+source devel/setup.bash;
+rosrun pick_objects pick_objects_test" &
